a5397414ca10199afa646213ade5019550a5cc16
Daniel Shi <8574277@qq.com>
2022-08-25 08:44:00 +0800

    add spring cloud and consul

--- capol-workflow-server/src/main/java/com/capol/workflow/server/WorkflowApplication.java
+++ capol-workflow-server/src/main/java/com/capol/workflow/server/WorkflowApplication.java
@@ -3,9 +3,11 @@ package com.capol.workflow.server;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableProcessApplication
+@EnableDiscoveryClient
 public class WorkflowApplication {
 
     public static void main(String[] args) {
